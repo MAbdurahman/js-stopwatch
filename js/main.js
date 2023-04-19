61bed4d16ef1c947821e20d17248d7c77118242f
MAbdurahman <mdbdrrhm@gmail.com>
2023-04-19 06:45:29 -0500

    Add preloader.gif with its styles and scripts

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+  // makes sure that whole site is loaded
+  $('#preloader-gif, #preloader').fadeOut(5000, function () {});
+});
+
+/*=============================================
+         js-voice-note-app scripts
+================================================*/
+$(function () {
+  
+  console.log('page has loaded');
+});
