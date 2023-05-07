2aaa2ecdba0244cd79fb20b6d798bac9cb1c4470
MAbdurahman <mdbdrrhm@gmail.com>
2023-05-07 08:50:27 -0500

    Add scripts for the carousel slider; Update the style for the carousel indicators

--- js/main.js
+++ js/main.js
@@ -10,6 +10,25 @@ $(window).on('load', function () {
          js-voice-note-app scripts
 ================================================*/
 $(function () {
+  //**************** variables ****************//
+  const slider = document.getElementById('slider');
+  let indicators = document.querySelectorAll('.indicator');
+  let carousel_index = 0;
   
-  console.log('page has loaded');
+  /**
+   * @description - adds an EventListener with an anonymous function that updates the
+   * indicator with the selected class and removes the selected class from the appropriate
+   * indicator. Also, add the transform style to slide the slider.
+   * @param {element}
+   * @param {number}
+   */
+  indicators.forEach(function (indicator, index) {
+    indicator.addEventListener('click', function (e) {
+      carousel_index = index;
+      document.querySelector('.indicator.selected').classList.remove('selected');
+      indicator.classList.add('selected');
+      slider.style.transform = 'translate(' + carousel_index * -50 + '%)';
+    })
+  });
+  
 });
